92f5a3589ac778f504f831e0dd9f856618e12007
BarrySpidster <silverhawk1195@gmail.com>
2018-01-30 23:13:11 +0400

    Изменены отступы

--- app/src/main/java/GameClasses/PositionChangeListener.java
+++ app/src/main/java/GameClasses/PositionChangeListener.java
@@ -3,9 +3,14 @@ package GameClasses;
 
 public interface PositionChangeListener {
     void moveRight();
+
     void moveLeft();
+
     void jump();
+
     void sitDown();
+
     void stopMoveHor();
+
     void stopMoveVer();
 }
