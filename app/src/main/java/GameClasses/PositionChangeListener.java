7e3bae9952e00fecbc06187da0e3540863dbe52f
BarrySpidster <silverhawk1195@gmail.com>
2018-01-30 22:45:49 +0400

    Описан интерфейс PositionChangeListener

--- app/src/main/java/GameClasses/PositionChangeListener.java
+++ app/src/main/java/GameClasses/PositionChangeListener.java
@@ -2,4 +2,10 @@ package GameClasses;
 
 
 public interface PositionChangeListener {
+    void moveRight();
+    void moveLeft();
+    void jump();
+    void sitDown();
+    void stopMoveHor();
+    void stopMoveVer();
 }
